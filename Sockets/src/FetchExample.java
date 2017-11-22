c20e6b5a1fd76d8669a55a0fa710cbcccac77564
Sami Rollins <srollins@cs.usfca.edu>
2017-11-22 11:09:39 -0800

    adding socket example

---
+++ Sockets/src/FetchExample.java
@@ -0,0 +1,20 @@
+public class FetchExample {
+
+	
+	public static void main(String[] args) {		
+		//test out the HTTPFetcher
+//		String page = HTTPFetcher.download("www.cs.usfca.edu", "/~srollins/test.html");
+//		
+		String apiKey = "YOUR_API_KEY";
+		String artist = "Madonna";
+		String track = "Holiday";
+		String page = HTTPFetcher.download("ws.audioscrobbler.com", "/2.0?artist=" + artist + 
+						"&track=" + track + 
+						"&api_key=" + apiKey + 
+						"&method=track.getInfo&format=json");
+		
+		System.out.println(page);
+		
+	}
+
+}
